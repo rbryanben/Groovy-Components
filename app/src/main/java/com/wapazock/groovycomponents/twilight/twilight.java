aea1bf9e64d84c7c3db1030a83d1779495b142da
Ryan Ben <63599157+rbryanben@users.noreply.github.com>
2020-10-23 06:28:40 +0200

    twilight component

--- app/src/main/java/com/wapazock/groovycomponents/twilight/twilight.java
+++ app/src/main/java/com/wapazock/groovycomponents/twilight/twilight.java
@@ -1,15 +1,19 @@
-package com.wapazock.groovycomponents;
+package com.wapazock.groovycomponents.twilight;
 
 import android.content.Context;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.wapazock.groovycomponents.R;
 
 /*
    This is the class for twilight Edit Text , which is part of the Groovy Components
@@ -35,20 +39,24 @@ public class twilight extends FrameLayout {
     //references
     private EditText input ;
     private RelativeLayout marker ;
+    private TextView header ;
 
     /*
       This initialization method will set all references and variables as well as the presets
      */
     private void  init(AttributeSet set){
           //set view
-          inflate(getContext(),R.layout.twilight_textbox,this);
+          inflate(getContext(), R.layout.twilight_textbox,this);
 
           //reference
           input = findViewById(R.id.textInput);
           marker = findViewById(R.id.marker);
+          header = findViewById(R.id.textView);
 
           //presets
-          marker.setVisibility(GONE); //hide marker
+          marker.setAlpha(0f); //hide marker
+          header.setScaleX(0.8f); // shrink header
+          header.setScaleY(0.8f);
 
           /*
              When user starts typing show marker , and clear when user clears text
@@ -62,10 +70,15 @@ public class twilight extends FrameLayout {
               @Override
               public void onTextChanged(CharSequence s, int start, int before, int count) {
                       if (input.getText().toString().isEmpty()){
-                          marker.setVisibility(GONE);
+                         marker.animate().alpha(0f).setDuration(100); //animate to 0 marker
+                          animateHeader(100,0.8f); //shrink and hide header
+
+
                       }
                       else {
-                          marker.setVisibility(VISIBLE);
+                          marker.animate().alpha(1f).setDuration(100); // animate to 1 marker
+
+                          animateHeader(100,1f); //enlarge header
                       }
               }
 
@@ -75,5 +88,15 @@ public class twilight extends FrameLayout {
               }
           });
 
+
+    }
+
+
+    /*
+       This function will animate the header
+     */
+    public void animateHeader(int duration , float scale){
+         header.animate().scaleY(scale).setDuration(duration);
+         header.animate().scaleX(scale).setDuration(duration);
     }
 }
